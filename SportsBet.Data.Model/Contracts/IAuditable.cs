96fef368e3f6ac4ca7b59b1cabfa42a13daecddb
Yasen Ivanov <yasen.denchev@gmail.com>
2018-04-24 17:02:38 +0300

    added data layer model

---
+++ SportsBet.Data.Model/Contracts/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsBet.Data.Model.Contracts
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedOn { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
---
+++ SportsBet.Data.Model/Models/Event.cs
@@ -0,0 +1,38 @@
+﻿using SportsBet.Data.Model.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsBet.Data.Model
+{
+    public class Event : DataModel
+    {
+        public Event(int eventID,
+                     string name,
+                     double oddsForFirstTeam,
+                     double oddsForDraw,
+                     double oddsForSecondTeam,
+                     DateTime startDate)
+        {
+            this.EventID = eventID;
+            this.Name = name;
+            this.OddsForFirstTeam = oddsForFirstTeam;
+            this.OddsForDraw = oddsForDraw;
+            this.OddsForSecondTeam = oddsForSecondTeam;
+            this.StartDate = startDate;
+        }
+        public int EventID { get; set; }
+
+        public string Name { get; set; }
+
+        public double OddsForFirstTeam { get; set; }
+
+        public double OddsForDraw { get; set; }
+
+        public double OddsForSecondTeam { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
